57c7b76c6722e41d90765dd3cc539d75b73c36ea
Nagaprasad13 <nagaprasad.013.03@gmail.com>
2026-09-20 21:54:04 +0530

    Time: 11 ms (66.49%), Space: 46.3 MB (86.57%) - LeetHub

---
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int findPairs(int[] nums, int k) {
+        if(k<0){
+            return 0;
+        }
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        int count=0;
+        for(int num:map.keySet()){
+            if(k==0){
+                if(map.get(num)>1){
+                    count++; 
+                }
+            }//duplicate value is counted when dis between the pair is zero and so when every has element has duplicate it is counted once
+                else{
+                    if(map.containsKey(num+k)){
+                        count++;
+                    }
+                }
+            
+        }
+        return count;
+    }
+}
